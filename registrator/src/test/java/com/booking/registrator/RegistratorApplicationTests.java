2b86840dbe858b8dcb5b9ce3003ec963c2a0efdc
Ultramar <artyom.odinochenko@yandex.ru>
2026-03-07 16:48:17 +0300

    Initial commit

---
+++ registrator/src/test/java/com/booking/registrator/RegistratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.registrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
